148e2deeef0afe5bacf4a6e8f498437d6f186685
Anej Gorkič <anej.gorkic@gmail.com>
2024-01-31 10:54:08 +0100

    Cleanup unused dev deps

--- build.sh
+++ build.sh
@@ -9,6 +9,5 @@ tsc --project tsconfig.json
 # Copy static files to 'dist' directory
 cp -r src/views dist/views
 cp -r src/public dist/public
-browserify src/public/main.js -o dist/public/main.js
 
 echo "Build complete."
